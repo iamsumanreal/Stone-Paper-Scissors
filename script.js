798da125c6e7d2a0f1ec7781e870f32c74d25780
iamsumanreal <129663740+iamsumanreal@users.noreply.github.com>
2024-06-22 17:35:53 +0530

    Final Update 2.0

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const paper = document.getElementById('paper');
 const scissor = document.getElementById('scissor');
 const stone = document.getElementById('stone');
+const playerChoiceImg = document.getElementById('playerChoiceImg');
+const aiChoiceImg = document.getElementById('aiChoiceImg');
+const resultElement = document.getElementById('result');
 
 let array = ["Paper", "Scissor", "Stone"];
 let score1 = 0;
 let score2 = 0;
 let lastChoice = null;
-
+resultElement.style.backgroundColor = "#5B8FB9";
 function getUniqueChoice() {
     let choise;
     do {
@@ -20,32 +23,34 @@ function handleClick(playerChoice) {
     const choise = getUniqueChoice();
     console.log(choise);
 
+    playerChoiceImg.src = `Assests/${playerChoice.toLowerCase()}.png`;
+
+    aiChoiceImg.src = `Assests/${choise.toLowerCase()}.png`;
+
     if (playerChoice === choise) {
-        document.getElementById("result").innerHTML = "It's Draw";
+        resultElement.innerHTML = "It's Draw";
+        resultElement.style.backgroundColor = "#FFD93D";
     } else if ((playerChoice === "Paper" && choise === "Stone") ||
                (playerChoice === "Stone" && choise === "Scissor") ||
                (playerChoice === "Scissor" && choise === "Paper")) {
-        document.getElementById("result").innerHTML = "You Win";
-        score1 += 2;
-        score2 -=1;
+                resultElement.innerHTML = "You won";
+                resultElement.style.backgroundColor = "#06D001";
+        score1 += 1;
+        score2 -= 1;
     } else {
-        document.getElementById("result").innerHTML = "You Lose";
+        resultElement.innerHTML = "You Lose";
+        resultElement.style.backgroundColor = "#FF204E";
         score1 -= 1;
-        score2 +=2;
+        score2 += 1;
     }
 
-    document.getElementById("myresult").innerHTML = playerChoice;
-    document.getElementById("aichoice").innerHTML = choise;
-    document.getElementById("Score1").innerHTML = "Your Score:" + score1;
-    document.getElementById("Score2").innerHTML = "AI's Score:" + score2;
+    document.getElementById("Score1").innerHTML = "Your Score: " + score1;
+    document.getElementById("Score2").innerHTML = "AI's Score: " + score2;
 
-    if(score1==10)
-        {
-            alert("You Won The Game!");
-            window.location.reload();
-        }
-
-    else if(score2==10){
+    if (score1 >= 10) {
+        alert("You Won The Game!");
+        window.location.reload();
+    } else if (score2 >= 10) {
         alert("AI Won The Game!");
         window.location.reload();
     }
